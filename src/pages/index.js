d93705382f9fb37132a36b1448d1b5d26910d3aa
pogojam <rlb278@nau.edu>
2019-10-30 12:32:22 -0700

    Changed intersection observer margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -257,7 +257,7 @@ const Scean_Interface = ({ index, ...props }) => {
 
   const [ref, entries] = useObserver({
     threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-    rootMargin: "30px 0px 0px 0px",
+    rootMargin: "0px 0px 0px 0px",
   })
   const [isActive, setActive] = useState(false)
 

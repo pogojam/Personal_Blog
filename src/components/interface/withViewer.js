fc574d54ba4598c7142fa1be51b84f3d6aab117f
Ryan Breaux <ryanbreaux@ryans-imac.lan>
2020-10-14 13:46:51 -0700

    Setting window height manualy

--- src/components/interface/withViewer.js
+++ src/components/interface/withViewer.js
@@ -379,7 +379,7 @@ const Content = ({
         style={{
           willChange: "transform opacity",
           position: "absolute",
-          zIndex: 0,
+          zIndex: 1,
           right: 0,
           bottom: 0,
           display: "flex",
